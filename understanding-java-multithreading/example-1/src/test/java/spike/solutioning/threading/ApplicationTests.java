7317e211f762a3818998e4a2f93137b55302cb1f
alfonsussetiawanjacub <out2240@astrapay.com>
2024-12-20 19:29:11 +0700

    feat(init-project) : init node-axios dan springboot

---
+++ understanding-java-multithreading/example-1/src/test/java/spike/solutioning/threading/ApplicationTests.java
@@ -0,0 +1,13 @@
+package spike.solutioning.threading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
